031f9760e89ee82c8cd2287ddcd13a46c1868023
Dasindu02 <163329767+Dasindu02@users.noreply.github.com>
2025-11-23 20:11:35 +0530

    create dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "../src/Pages/Register";
 import Login from "../src/Pages/Login";
+import Dashboard from "./Pages/Dashborad";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
-      </Routes>
+      
 
-      <Routes>
+      
         <Route path="/" element={<Login />} />
+      
+
+      
+        <Route path="/ModiFyX-Dashboard" element={<Dashboard />} />
       </Routes>
 
     </BrowserRouter>
---
+++ src/Pages/Dashborad.tsx
@@ -0,0 +1,12 @@
+const Dashboard = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white shadow-lg p-10 rounded-xl text-center">
+        <h1 className="text-green-700 font-bold mb-2">Hi, this is your Dashboard</h1>
+        <p className="text-red-500">Welcome back to ModiFyX!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
