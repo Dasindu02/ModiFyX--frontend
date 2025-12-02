baf43a98788fa7d242f6df716fd3316a9fbee1f0
Dasindu02 <163329767+Dasindu02@users.noreply.github.com>
2025-12-02 19:41:28 +0530

    add the back btn

--- src/Pages/Technologies.tsx
+++ src/Pages/Technologies.tsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import img1 from "../assets/backg.jpg";
 import interfaceApp from "../assets/interface app.png"; 
+import { Link, useNavigate } from "react-router-dom";
+
 
 const Technologies: React.FC = () => {
   const [currentBg, setCurrentBg] = useState(0);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const navigate = useNavigate();
+
   
   const backgroundImages = [img1];
 
@@ -103,12 +107,36 @@ const Technologies: React.FC = () => {
 
   return (
     <div className="min-h-screen fixed inset-0 bg-black text-white overflow-x-hidden">
+      
       {/* Animated Background */}
       <div
         key={currentBg}
         className="fixed inset-0 bg-cover bg-center transition-all duration-[1500ms] animate-slide"
         style={{ backgroundImage: `url(${backgroundImages[currentBg]})` }}
       ></div>
+      
+       <button
+        onClick={() => navigate(-1)}
+        className="fixed top-4 mt-16 left-4 z-50 bg-black/40 backdrop-blur-md p-3 rounded-full 
+                  hover:bg-black/60 transition shadow-lg border border-white/20
+                  lg:absolute lg:top-6 lg:left-6"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="white"
+          className="w-5 h-5 lg:w-6 lg:h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 19l-7-7 7-7"
+          />
+        </svg>
+      </button>
+
 
       {/* Dark Overlay */}
       <div className="fixed inset-0 bg-black bg-opacity-60"></div>
